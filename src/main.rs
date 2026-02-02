56b956e78dd9c6e982cb6ba905c6ab38be6ce4b5
Swift <swiftshineexists@gmail.com>
2026-02-02 16:52:47 -0600

    json conversion

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,65 @@
 mod animate;
 mod mneb;
 use anyhow::Result;
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand};
 use std::fs;
 
-#[derive(Clone, Debug, ValueEnum)]
+#[derive(Subcommand, Debug)]
 enum Usage {
-    Animate,
-    // Convert,
+    Animate {
+        filename: String,
+        #[arg(short, long, default_value_t = 60.0f32)]
+        framerate: f32,
+    },
+    Convert {
+        filename: String,
+        #[arg(default_value_t = String::from("out.json"))]
+        output_json: String,
+        /// Make the JSON output pretty.
+        #[arg(short, long, default_value_t = false)]
+        pretty: bool,
+    },
 }
 
 #[derive(Parser, Debug)]
+#[command(subcommand_precedence_over_arg = true)]
 struct Args {
+    /// The action to perform.
+    #[command(subcommand)]
     usage: Usage,
-    mneb_file: String,
-    output_json: Option<String>,
-    framerate: Option<f32>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let file = fs::read(args.mneb_file)?;
-    let mneb_file = mneb::MNEBFile::from_bytes(&file)?;
-
-    match args.usage {
-        Usage::Animate => {
+    match &args.usage {
+        Usage::Animate {
+            framerate,
+            filename,
+        } => {
+            let file = fs::read(filename)?;
+            let mneb_file = mneb::MNEBFile::from_bytes(&file)?;
             if mneb_file.has_curves() {
-                let framerate = args.framerate.unwrap_or_else(|| 60.0f32);
-                animate::play_animation(mneb_file, framerate);
+                animate::play_animation(mneb_file, *framerate);
             } else {
                 // nothing to do
                 println!("File does not have curves to render.");
             }
+        }
+
+        Usage::Convert {
+            output_json,
+            filename,
+            pretty,
+        } => {
+            let file = fs::read(filename)?;
+            let mneb_file = mneb::MNEBFile::from_bytes(&file)?;
+            let json = if *pretty {
+                serde_json::to_string_pretty(&mneb_file)?
+            } else {
+                serde_json::to_string(&mneb_file)?
+            };
+            fs::write(output_json, json)?;
         }
     }
 
